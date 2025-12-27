90338e7af33e920ff197dd922aadcd66d30de88b
samuelsantana-dev <samuelsantanadasilva8@gmail.com>
2025-12-27 11:57:35 -0300

    ♻️ refactor: separar regras de negócio dos controllers para os services

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,24 @@
+import { AuthService } from './../services/auth.service';
+import { Request, Response } from "express";
+
+const service = new AuthService();
+
+export const register = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    const user = await service.register(email, password);
+    res.json({ message: "Usuário registrado com sucesso!", user });
+  } catch (error) {
+    res.status(500).json({ error: "Erro no servidor" });
+  }
+};
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    const token = await service.login(email, password);
+    res.json({ token });
+  } catch (err: any) {
+    res.status(400).json({ error: err.message });
+  }
+};
---
+++ backend/src/controllers/order.controller.ts
@@ -0,0 +1,40 @@
+import { OrderService } from "../services/order.service";
+import { Request, Response } from "express";
+
+const service = new OrderService();
+
+export const createOrder = async (req: Request, res: Response) => {
+  try {
+    const order = await service.createOrder(req.body);
+    res.json({ message: "Pedido criado com sucesso!", order });
+  } catch (error) {
+    res.status(500).json({ error: "Erro no servidor" });
+  }
+};
+
+export const listOrders = async (req: Request, res: Response) => {
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const state = req.query.state as string || "";
+
+  try {
+    const orders = await service.listOrders(page, limit, state);
+    res.json({ message: "Pedidos listados com sucesso!", orders });
+  } catch (error) {
+    res.status(500).json({ error: "Erro no servidor" });
+  }
+
+};
+
+export const advanceOrder = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    if(!id) {
+      return res.status(400).json({ error: "ID do pedido é obrigatório" });
+    }
+    const result = await service.advanceOrder(id);
+    res.json(result);
+  } catch (err: any) {
+    res.status(400).json({ error: err.message });
+  }
+};
